a72bdc1f1e1c2466836b2371e65ccf35e8bd2e93
victor <victordj587@gmail.com>
2022-03-28 17:41:09 +0200

    test

--- sketch.js
+++ sketch.js
@@ -33,6 +33,9 @@ function draw() {
   textAlign(CENTER);
   textSize(31);
   haswon() ? text("you win, the word was \n '" + word.word + "'",width/2,height/1.3) : null;
+  stroke(255,100);
+  drawBorder();
+  drawMan()
 }
 
 function drawLetters(color) {
@@ -60,10 +63,16 @@ function drawLetters(color) {
 }
 
 function keyTyped() {
+  let letterFound = false;
   for(let i = 0 ; i < word.letters.length ; i++) {
     if(key.toLowerCase() === word.letters[i].toLowerCase()) {
+      letterFound = true;
       word.guessed[i] = true;
     }
+  }
+  if(!letterFound) { 
+    guessesLeft--;
+    print(guessesLeft);
   }
 }
 
@@ -74,5 +83,23 @@ function haswon() {
     }
   }
   print(word);
+  noLoop();
   return true;
+}
+
+function drawBorder() {
+  stroke(255,100);
+  strokeWeight(4);
+  line(0,height/1.5,width,height/1.5);
+  line(width/2,height/1.5,width/2,0);
+}
+
+function drawMan() {
+  let C2 = createVector(width/2,height/1.5);
+  stroke(255,100);
+  strokeWeight(10);
+  //point(C2.x,C2.y);
+  if(guessesLeft <= 9) {
+    circle(C2.x+150,C2.y-300,100);
+  }
 }
